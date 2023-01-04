99a7966b415b6e98c3f73a1d2701f1ad96dd9963
git-lyg <liuyuguoyxl@163.com>
2023-01-04 15:24:17 +0800

    Create Person.java
    add Person class

---
+++ Person.java
@@ -0,0 +1,38 @@
+public class Person {
+  private Integer id;
+  private Integer age;
+  private String name;
+  private String addr;
+  
+  public void setId(Integer id) {
+    this.id = id;
+  }
+  
+  public Integer getId() {
+    return id;
+  }
+  
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+  
+  public Integer getAge() {
+    return age;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setAddr(String addr) {
+    this.addr = addr; 
+  }
+  
+  public String getAddr() {
+    return addr;
+  }
+}
